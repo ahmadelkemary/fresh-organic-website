f11af01a32fb74adfdb7a16d2be50d80ad5ad99f
Ahmad Elkemary <107653774+ahmadelkemary@users.noreply.github.com>
2022-09-08 14:11:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,22 +19,22 @@ window.onscroll = () =>{
 
 }
 
-let countDate = new Date('june 1, 2021 00:00:00').getTime();
+var countDate = new Date('Jan 5, 2024 15:37:25').getTime();
 
 function CountDown(){
 
-    let now = new Date().getTime();
+    var now = new Date().getTime();
     gap = countDate - now;
 
-    let second = 1000;
-    let minute = second * 60;
-    let hour = minute * 60;
-    let day = hour * 24;
+    var second = 1000;
+    var minute = second * 60;
+    var hour = minute * 60;
+    var day = hour * 24;
 
-    let d = Math.floor(gap / (day));
-    let h = Math.floor((gap % (day)) / (hour));
-    let m = Math.floor((gap % (hour)) / (minute));
-    let s = Math.floor((gap % (minute)) / (second));
+    var d = Math.floor(gap / (day));
+    var h = Math.floor((gap % (day)) / (hour));
+    var m = Math.floor((gap % (hour)) / (minute));
+    var s = Math.floor((gap % (minute)) / (second));
 
     document.getElementById('day').innerText = d;
     document.getElementById('hour').innerText = h;
